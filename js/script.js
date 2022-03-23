42a5e616e2d09b04f546e5e6f06af627d72907df
yuri <yuri.vynnyk@gmail.com>
2022-03-23 19:21:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+let num = 5; //число
+console.log(4/0); //бесконечность infinity
+console.log('string' * 9); //Nan (not a number)
+const string = '5'; //строка
+const bool = true; //boolean
+// console.log(something); //что то чего не существует null
+let und; //что то существует, но нет у него значения undefined
+
+const obj = {
+  name: 'Yuri',
+  age: 25,
+  isMarried: false
+};
+
+console.log(obj.age);
+// console.log(obj['age']);
+
+let arr = ['plumb.png', 'orange.jpg', 'apple.bmp', {}, [], 'string'];
+//массив частный слуйчай обьекта, не отдельный тип данных
+console.log(arr[0]);
+
+
+// alert('Hiiiii');
+
+// const result = confirm('Are u here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '');
+// console.log(typeof(answer));// вся информация от пользователя которую получаем это будет строка
+
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(answer + 5);// вся информация от пользователя которую получаем это будет строка
+
+const answers = [];
+
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+
+console.log(typeof(answers));
+
+const category = 'toys';
+console.log(`htttp://someurl.com/${category}/5` );
+
+const user = 'Ivan';
+alert(`Hi, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// console.log(incr++); // 10
+// console.log(decr++); // 10
+
+console.log(++incr); // 11
+console.log(--decr); // 9
+
+console.log(2*8 === '8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose); // false, && - и
+console.log(isChecked || isClose); // true, || - или 
+console.log(isChecked && !isClose); // true, восклицательный это обратное
