ee193db0324a4dcc6715e29f6e3ef1c4fc1adbd1
yuri <vagabund1990@gmail.com>
2022-06-16 07:09:09 +0300

    finish Lesson 53

--- JS продвинутый/Lesson53/server.php
+++ JS продвинутый/Lesson53/server.php
@@ -1,2 +1,3 @@
 <?php
+$_POST = json_decode(file_get_contents("php://input"), true); //php не умеет работать с данныеми json. всё что приходит от клиента декадируем от json и тогда можно рабоать
 echo var_dump($_POST);
