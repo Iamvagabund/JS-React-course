7e663b2562fc7f2a4acb493d0b217a046d48f9ae
yuri <yuri.vynnyk@gmail.com>
2022-08-09 14:25:09 +0300

    add lesson 58

--- JS продвинутый/Lesson58/js/script.js
+++ JS продвинутый/Lesson58/js/script.js
@@ -233,11 +233,23 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
   });
 
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: data
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -257,14 +269,7 @@ window.addEventListener('DOMContentLoaded', () => {
         object[key] = value;
       });
 
-      fetch('server.php', {
-        method: "POST",
-        header: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(object)
-      })
-      .then(data => data.text())
+      postData('http://localhost:3000/requests', JSON.stringify(object))
       .then(data => {
         console.log(data);
         showThanksModal(message.success);
